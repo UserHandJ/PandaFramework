f1e019a3616be802bd7b846f1b2890b51ed57b32
何锦贤 <3473206245@qq.com>
2023-07-28 16:55:55 +0800

    添加场景管理模块

---
+++ Manager/SceneMgr/SceneMgr.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// 场景切换模块,
+/// </summary>
+public class SceneMgr : BaseSingleton<SceneMgr>
+{
+    /// <summary>
+    /// 同步
+    /// </summary>
+    /// <param name="sceneName"></param>
+    /// <param name="Callback"></param>
+    public void LoadScene(string sceneName, UnityAction Callback = null)
+    {
+        SceneManager.LoadScene(sceneName);
+        Callback?.Invoke();
+    }
+
+    /// <summary>
+    /// 异步
+    /// </summary>
+    /// <param name="sceneName"></param>
+    /// <param name="Callback"></param>
+    public void LoadSceneAsyn(string sceneName, UnityAction Callback = null)
+    {
+        MonoMgr.Instance.StartCoroutine(ReallyLoadSceneAsyn(sceneName, Callback));
+    }
+    private IEnumerator ReallyLoadSceneAsyn(string sceneName, UnityAction Callback = null)
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(sceneName);
+        while(!ao.isDone)
+        {
+            // 事件中心 向外分发 进度情况  外面想用就用
+            EventCenter.Instance.EventTrigger("", ao.progress);
+            yield return ao.progress;
+        }
+        Callback?.Invoke();
+    }
+}
